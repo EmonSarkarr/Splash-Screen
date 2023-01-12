8637715b8865c932ceb92db847f595f3b5d89874
EMON <emonsarkar1111@gmail.com>
2023-01-12 14:32:29 +0600

    Alert Dialog message option

--- app/src/main/java/com/example/spalashscreenpractice/HomeActivity.java
+++ app/src/main/java/com/example/spalashscreenpractice/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.spalashscreenpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,5 +12,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setMessage("Are you sure you want to EXIT ??")
+                .setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).setNegativeButton("No",null).show();
+
+
+
     }
 }
